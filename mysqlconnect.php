40bff6143328712ab4bef27d6b2518e6a9c1978a
Han-Mi Kim <tmpham1@uci.edu>
2018-10-22 19:07:22 -0700

    create media queries land filled whitespace

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,9 +1,9 @@
 <?php
-$conn = mysqli_connect("host", "user", "password", "database");
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+// $conn = mysqli_connect("host", "user", "password", "database");
+// if (mysqli_connect_errno())
+//   {
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   }
 
 
 
